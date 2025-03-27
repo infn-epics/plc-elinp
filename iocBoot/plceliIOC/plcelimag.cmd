31c2526127cf34de9ad4a066c8be46bf552662ed
amichelo <andrea.michelotti@lnf.infn.it>
2025-03-27 11:59:20 +0100

    added plceli

---
+++ iocBoot/plceliIOC/plcelimag.cmd
@@ -0,0 +1,18 @@
+#!../../bin/linux-x86_64/plceli
+
+< envPaths
+
+## Register all support components
+dbLoadDatabase "../../dbd/plceli.dbd"
+plceli_registerRecordDeviceDriver(pdbbase)
+
+
+drvAsynIPPortConfigure("PLCELI_IP", "10.16.4.68:502", 0, 0, 0)
+modbusInterposeConfig("PLCELI_IP",0,0,0)
+
+drvModbusAsynConfigure("PLCELI_IO", "PLCELI_IP", 1,    3, 0, 5, 0, 1000, "PLCEL")
+
+dbLoadRecords("$(TOP)/db/plcelimag.template","P=LEL,R=PLCMAG,PORT=PLCELI_IO")
+
+iocInit()
+
