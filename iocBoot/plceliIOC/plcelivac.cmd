31e94dba9be8dd0265fe2f47b8002738de84e9c7
amichelo <andrea.michelotti@lnf.infn.it>
2025-12-03 11:45:28 +0200

    updated plc

--- iocBoot/plceliIOC/plcelivac.cmd
+++ iocBoot/plceliIOC/plcelivac.cmd
@@ -7,10 +7,10 @@ dbLoadDatabase "../../dbd/plceli.dbd"
 plceli_registerRecordDeviceDriver(pdbbase)
 
 
-drvAsynIPPortConfigure("PLCELI_IP", "10.16.4.68:502", 0, 0, 0)
+drvAsynIPPortConfigure("PLCELI_IP", "10.16.4.170:502", 0, 0, 0)
 modbusInterposeConfig("PLCELI_IP",0,0,0)
 
-drvModbusAsynConfigure("PLCELI_IO", "PLCELI_IP", 1,    3, 0, 5, 0, 1000, "PLCEL")
+drvModbusAsynConfigure("PLCELI_IO", "PLCELI_IP", 1,    3, 0, 10, 0, 1000, "PLCEL")
 
 dbLoadRecords("$(TOP)/db/plcelivac.template","P=LEL,R=PLCMAG,PORT=PLCELI_IO")
 
